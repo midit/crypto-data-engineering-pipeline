9442e48666c38f67cb0bd42831a8a97abc701175
Maksym <46499085+midit@users.noreply.github.com>
2026-03-11 18:44:00 +0000

    add transformation layer for crypto market data

--- data_pipeline/ingest.py
+++ data_pipeline/ingest.py
@@ -1,25 +1,31 @@
 import requests
 import pandas as pd
+import logging
+from transform import transform_crypto_data
 
-def fetch_crypto_data():
+logging.basicConfig(level=logging.INFO)
+
+def fetch_crypto_data(limit=20):
     url = "https://api.coingecko.com/api/v3/coins/markets"
 
     params = {
         "vs_currency" : "usd",
         "order": "market_cap_desc",
-        "per_page": 20,
+        "per_page": limit,
         "page": 1,
         "sparkline": False
     }
 
     response = requests.get(url, params=params)
-
-    if response.status_code != 200:
-        raise Exception(f"API request failed with status code {response.status_code}")
+    response.raise_for_status()
 
     data = response.json()
 
     df = pd.DataFrame(data)
+    df = df[df["symbol"].notnull()]
+    df.fillna({"current_price":0, "market_cap":0, "total_volume":0}, inplace=True)
+    
+    logging.info("Fetching %d rows", len(df))
 
     df = df[
         [
@@ -29,9 +35,16 @@ def fetch_crypto_data():
             "total_volume",
         ]
     ]
+
+    df["current_price"] = df["current_price"].round(2)
+    df["market_cap"] = (df["market_cap"] / 1e9).round(2)
+    df["total_volume"] = (df["total_volume"] / 1e9).round(2)
     
     return df
 
 if __name__ == "__main__":
-    crypto_df = fetch_crypto_data()
-    print(crypto_df.head())
+    df = fetch_crypto_data()
+    df = transform_crypto_data(df)
+
+    print(df.head())
+    print(df.dtypes)
